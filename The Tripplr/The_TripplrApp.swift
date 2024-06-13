09b1ce0b9c9812f1b2975d167b19d4a25b17b296
Simbalionet <harsha0274u@gmail.com>
2024-06-13 15:32:19 +0530

    Initial Commit

---
+++ The Tripplr/The_TripplrApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_TripplrApp.swift
+//  The Tripplr
+//
+//  Created by Harsha Reddy on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct The_TripplrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
